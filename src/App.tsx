6f202f276b2e13210f63e0ab01b1a0eed1669111
Muhammad Iqbal <hellofromiqbal@gmail.com>
2025-05-07 00:43:16 +0700

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Container, CssBaseline } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Container
+        maxWidth='lg'
+        sx={{
+          bgcolor: 'lightblue',
+          height: '100dvh',
+        }}
+      ></Container>
+    </>
+  )
+}
+
+export default App
